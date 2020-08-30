22c7fe11c5211671545dd6e0fc7ff75c2cc69b1b
91805 <hritik.raheja_cs19@gla.ac.in>
2020-08-30 13:05:30 +0530

    Created Student Class With Constructors, toString method and all other required methods.

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -6,5 +6,44 @@
  * */
 package definitions;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Library {
+    Book[] availableBooks;
+
+    public Library() {
+        this.availableBooks = new Book[25];
+        for (int indexNumber = 0; indexNumber < availableBooks.length; indexNumber++) {
+            availableBooks[indexNumber] = new Book("Book " + (indexNumber + 1));
+        }
+    }
+
+    public Library(Book[] availableBooks) {
+        this.availableBooks = availableBooks;
+    }
+
+    public String getAvailableBooks() {
+        return Arrays.toString(availableBooks);
+    }
+
+    public void setAvailableBooks(Book[] availableBooks) {
+        this.availableBooks = availableBooks;
+    }
+
+    public String toString() {
+        return String.format("ALL AVAILABLE BOOKS ARE : %s", Arrays.toString(availableBooks));
+    }
+
+    public int hashCode() {
+        return Objects.hash(getAvailableBooks());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Arrays.equals(availableBooks, library.availableBooks);
+    }
 }
